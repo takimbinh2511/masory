f1fd1c6958fada11505db4abc9f9d628450a2a5a
binh18520510 <18520510@gm.uit.edu.vn>
2021-10-02 21:54:09 +0700

    update code

---
+++ isotope.js
@@ -0,0 +1,22 @@
+﻿ $(function(){
+    $hieuung =$('.noidung').isotope({
+        // options
+        itemSelector: '.khoianh',
+        layoutMode: 'masonry'
+    });
+
+     // layout Isotope after each image loads
+    $hieuung.imagesLoaded().progress( function(){
+      $hieuung.isotope('layout');
+    });
+    $('.danhmuc a').click(function(){
+      $('.danhmuc a').removeClass('duocchon');  
+      $(this).addClass('duocchon');
+ 
+      tendanhmuc = $(this).attr('href');
+      console.log(tendanhmuc);
+      $hieuung.isotope({filter:tendanhmuc})
+      return false;
+    })
+})  
+ 
